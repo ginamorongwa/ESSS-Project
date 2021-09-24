a86711bfa7dcff8845cb7057dc3c8b91c4bd5210
Virginia Mthimunye <ginamorongwa@gmail.com>
2021-09-24 09:03:29 +0200

    commit

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -6,13 +6,13 @@ import "@firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
 
-  apiKey: "AIzaSyCUSY7bU0tB8cxxnMo020-pVcgZT3ygw7k",
-  authDomain: "esssproj.firebaseapp.com",
-  projectId: "esssproj",
-  storageBucket: "esssproj.appspot.com",
-  messagingSenderId: "560492761933",
-  appId: "1:560492761933:web:b388a56210f7d2ade4dd04",
-  measurementId: "G-TC74PBG26W"
+  // apiKey: "AIzaSyCUSY7bU0tB8cxxnMo020-pVcgZT3ygw7k",
+  // authDomain: "esssproj.firebaseapp.com",
+  // projectId: "esssproj",
+  // storageBucket: "esssproj.appspot.com",
+  // messagingSenderId: "560492761933",
+  // appId: "1:560492761933:web:b388a56210f7d2ade4dd04",
+  // measurementId: "G-TC74PBG26W"
 
   /*apiKey: "AIzaSyDJNrRiujlkTCM11XYLmoiFzDUCVbSWTvU",
   authDomain: "esss-d5970.firebaseapp.com",
@@ -45,6 +45,15 @@ const firebaseConfig = {
   // messagingSenderId: "125175989653",
   // appId: "1:125175989653:web:cafbf73994c3d124ba229c",
   // measurementId: "G-VFHL44BZGE"
+
+  apiKey: "AIzaSyD8PBBOv_3uAMB_cDCEoWGHTy5ski7Y-74",
+  authDomain: "esss-project-88363.firebaseapp.com",
+  databaseURL: "https://esss-project-88363-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "esss-project-88363",
+  storageBucket: "esss-project-88363.appspot.com",
+  messagingSenderId: "829890238663",
+  appId: "1:829890238663:web:45ed796cc67ae1a0c3c6e2",
+  measurementId: "G-XDZJ5HSEDH"
 };
 let app;
 if (firebase.apps.length === 0) {
